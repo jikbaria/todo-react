3aa64a393e4abf20283c02057f7cd55a9469357d
jikbaria <jikbaria@gmail.com>
2025-08-08 13:39:23 +0300

    test: add vitest + rtl and a smoke test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="mx-auto mt-12 flex max-w-2xl flex-col gap-4 px-5">
+    <main className="mx-auto mt-12 flex max-w-2xl flex-col gap-4 px-5">
       <h1 className="text-4xl font-extrabold">My Tasks</h1>
-    </div>
+    </main>
   );
 }
 
